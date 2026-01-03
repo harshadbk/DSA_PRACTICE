cb446fe718c29ef374aa659144fea314830cbdd5
Harshad Bajirao Khatale <khataleharshad26@gmail.com>
2026-01-03 14:38:38 +0530

    Done Linked List

--- Linked_List/basic.cpp
+++ Linked_List/basic.cpp
@@ -1,7 +1,143 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+class node {
+public:
+    int data;
+    node* next;
+
+    node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class Linkedlist {
+private:
+    node* first;
+    node* last;
+
+public:
+    Linkedlist() {
+        first = nullptr;
+        last = nullptr;
+    }
+
+    void push_front(int val) {
+        node* newnode = new node(val);
+        if (first == nullptr) {
+            first = last = newnode;
+        } else {
+            newnode->next = first;
+            first = newnode;
+        }
+    }
+
+    void push_back(int val) {
+        node* newnode = new node(val);
+        if (first == nullptr) {
+            first = last = newnode;
+        } else {
+            last->next = newnode;
+            last = newnode;
+        }
+    }
+
+    // insert at index
+    void insertpos(int idx, int val) {
+        if (idx == 0) {
+            push_front(val);
+            return;
+        }
+
+        node* temp = first;
+        for (int i = 0; i < idx - 1; i++) {
+            if (temp == nullptr) {
+                cout << "Index out of bound\n";
+                return;
+            }
+            temp = temp->next;
+        }
+
+        node* newnode = new node(val);
+        newnode->next = temp->next;
+        temp->next = newnode;
+
+        if (newnode->next == nullptr) {
+            last = newnode;
+        }
+    }
+
+    void popfront() {
+        if (first == nullptr) return;
+
+        node* temp = first;
+        first = first->next;
+        delete temp;
+
+        if (first == nullptr)
+            last = nullptr;
+    }
+
+    void popback() {
+        if (first == nullptr) return;
+
+        if (first == last) {
+            delete first;
+            first = last = nullptr;
+            return;
+        }
+
+        node* temp = first;
+        while (temp->next != last) {
+            temp = temp->next;
+        }
+
+        delete last;
+        last = temp;
+        last->next = nullptr;
+    }
+
+    void printdata() {
+        node* head = first;
+        while (head != nullptr) {
+            cout << head->data << " -> ";
+            head = head->next;
+        }
+        cout << "NULL"<<endl;
+    }
+
+    int search(int val){
+        node *temp = first;
+        int i=0;
+        while(temp!=nullptr){
+            if(temp->data==val){
+                return i;
+            }
+            i++;
+            temp = temp->next;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Linkedlist li;
+    li.push_front(12);
+    li.push_front(78);
+    li.push_back(13);
+    li.push_back(89);
+    li.insertpos(2, 36);
+    li.insertpos(3, 1);
+    li.insertpos(6, 29);
+
+    li.printdata();
+
+    li.popfront();
+    li.popback();
+
+    li.printdata();
+    cout << li.search(36)<<endl;
+    li.printdata();
     return 0;
 }
---
+++ Linked_List/doubly_LL.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int no;
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        no = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class List
+{
+public:
+    node *first;
+    node *last;
+    List()
+    {
+        first = nullptr;
+        last = nullptr;
+    }
+    void push_front(int val)
+    {
+        node *newnode = new node(val);
+        if (first == nullptr)
+        {
+            first = last = newnode;
+            return;
+        }
+
+        newnode->next = first;
+        first->prev = newnode;
+        first = newnode;
+    }
+
+    void push_back(int val)
+    {
+        node *newnode = new node(val);
+
+        if (last == nullptr)
+        {
+            last = first = newnode;
+            return;
+        }
+
+        last->next = newnode;
+        newnode->prev = last;
+        last = newnode;
+    }
+
+    void insertbetween(int val,int pos){
+        node* newnode = new node(val);
+        if(pos==0){
+            if(first==nullptr){
+                first=last=newnode;
+                return;
+            }
+            else{
+                newnode->next = first;
+                first->prev = newnode;
+                first = newnode;
+                return;
+            }
+        }
+
+        int i = 0;
+        node* temp = first;
+        while (temp!=nullptr && i<pos-1)
+        {
+            temp = temp->next;
+            i++;
+        }
+
+        if(temp==nullptr){
+            cout << "Invalid Position:\n";
+            delete newnode;
+            return;
+        }
+
+        if(temp->next==nullptr){
+            temp->next = newnode;
+            newnode->prev = temp;
+            last = newnode;
+        }
+        else{
+            newnode->next = temp->next;
+            newnode->prev=temp;
+            temp->next->prev = newnode;
+            temp->next = newnode;
+        }
+    }
+
+    void printlist()
+    {
+        node *ptr = first;
+        while (ptr != nullptr)
+        {
+            cout << ptr->no << " -> ";
+            ptr = ptr->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    List l;
+    l.push_front(78);
+    l.push_front(11);
+    l.push_front(23);
+    l.push_back(45);
+    l.insertbetween(29,0);
+    l.insertbetween(9,7);
+    l.insertbetween(56,2);
+    l.insertbetween(71,6);
+    l.printlist();
+    return 0;
+}
